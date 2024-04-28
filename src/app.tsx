47a298dfb7f4f3b626f4e82f369fef85f51659a7
YUUU <894462643@qq.com>
2024-04-28 21:05:32 +0800

    前端项目初始化

---
+++ src/app.tsx
@@ -0,0 +1,131 @@
+import { AvatarDropdown, AvatarName, Footer, Question } from '@/components';
+import { currentUser as queryCurrentUser } from '@/services/ant-design-pro/api';
+import { LinkOutlined } from '@ant-design/icons';
+import type { Settings as LayoutSettings } from '@ant-design/pro-components';
+import { SettingDrawer } from '@ant-design/pro-components';
+import type { RunTimeLayoutConfig } from '@umijs/max';
+import { Link, history } from '@umijs/max';
+import defaultSettings from '../config/defaultSettings';
+import { requestConfig } from './requestConfig';
+import {getLoginUserUsingGet} from "@/services/Yapi-backend/userController";
+const isDev = process.env.NODE_ENV === 'development';
+const loginPath = '/user/login';
+
+/**
+ * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
+ * */
+export async function getInitialState(): Promise<InitialState> {
+  // 当页面首次加载时，获取要全局保存的数据，比如用户登录信息
+  const state: InitialState = {
+    // 初始化登录用户的状态，初始值设为undefined
+    loginUser: undefined,
+  }
+  try {
+    // 调用getLoginUserUsingGET()函数，尝试获取当前已经登录的用户信息
+    const res = await getLoginUserUsingGet();
+    // 如果从后端获取的数据不为空，就把获取到的用户数据赋值给state.loginUser
+    if (res.data) {
+      state.loginUser = res.data;
+    }
+    // 如果在获取用户信息的过程中发生错误，就把页面重定向到登录页面
+  } catch (error) {
+    history.push(loginPath);
+  }
+  // 返回修改后的状态
+  return state;
+};
+
+// ProLayout 支持的api https://procomponents.ant.design/components/layout
+export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) => {
+  return {
+    actionsRender: () => [<Question key="doc" />],
+    avatarProps: {
+      src: initialState?.loginUser?.userAvatar,
+      title: <AvatarName />,
+      render: (_, avatarChildren) => {
+        return <AvatarDropdown>{avatarChildren}</AvatarDropdown>;
+      },
+    },
+    waterMarkProps: {
+      content: initialState?.loginUser?.userName,
+    },
+    footerRender: () => <Footer />,
+    onPageChange: () => {
+      const { location } = history;
+      // 如果没有登录，重定向到 login
+      if (!initialState?.loginUser && location.pathname !== loginPath) {
+        history.push(loginPath);
+      }
+    },
+    bgLayoutImgList: [
+      {
+        src: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/D2LWSqNny4sAAAAAAAAAAAAAFl94AQBr',
+        left: 85,
+        bottom: 100,
+        height: '303px',
+      },
+      {
+        src: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/C2TWRpJpiC0AAAAAAAAAAAAAFl94AQBr',
+        bottom: -68,
+        right: -45,
+        height: '303px',
+      },
+      {
+        src: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/F6vSTbj8KpYAAAAAAAAAAAAAFl94AQBr',
+        bottom: 0,
+        left: 0,
+        width: '331px',
+      },
+    ],
+    links: isDev
+      ? [
+          <Link key="openapi" to="/umi/plugin/openapi" target="_blank">
+            <LinkOutlined />
+            <span>OpenAPI 文档</span>
+          </Link>,
+        ]
+      : [],
+    menuHeaderRender: undefined,
+    // 自定义 403 页面
+    // unAccessible: <div>unAccessible</div>,
+    // 增加一个 loading 的状态
+    childrenRender: (children) => {
+      // if (initialState?.loading) return <PageLoading />;
+      return (
+        <>
+          {children}
+          {isDev && (
+            <SettingDrawer
+              disableUrlParams
+              enableDarkTheme
+              settings={initialState?.settings}
+              onSettingChange={(settings) => {
+                setInitialState((preInitialState) => ({
+                  ...preInitialState,
+                  settings,
+                }));
+              }}
+            />
+          )}
+        </>
+      );
+    },
+    ...initialState?.settings,
+  };
+};
+
+/**
+ * @name request 配置，可以配置错误处理
+ * 它基于 axios 和 ahooks 的 useRequest 提供了一套统一的网络请求和错误处理方案。
+ * @doc https://umijs.org/docs/max/request#配置
+ */
+// export const request = {
+//   ...requestConfig,
+// };
+//使用了对象的展开语法（spread syntax），将一个名为requestConfig的对象中的所有属性复制到了一个新的对象中，并将这个新的对象赋给了request变量。
+// 这样做的结果是，request变量引用的是一个与requestConfig对象相同的对象，但是它们是两个不同的对象，即使它们具有相同的属性和值。
+
+export const request =requestConfig;
+//这个代码段简单地将request变量设置为requestConfig变量的值，没有创建新的对象。
+// 这意味着request和requestConfig引用的是同一个对象，而不是两个不同的对象。
+// 因此，对request的修改也会影响requestConfig，反之亦然。
